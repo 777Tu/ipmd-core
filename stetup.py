02e93da1f16a47e82b13cd7bb97fde3c12889803
777Tu <tuscottz7@gmail.com>
2026-04-20 23:05:01 +0100

    stetup added

---
+++ stetup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ipmd",
+    version="0.1.0",
+    description="Image Pixel MetaData - Pin metadata into image pixels",
+    author="777Tu",
+    packages=find_packages(),
+    install_requires=["pillow"],
+    
+)

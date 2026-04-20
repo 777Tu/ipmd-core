f65fc58f2f4e21843620e5b9fefcb9e78530eaf7
777Tu <tuscottz7@gmail.com>
2026-04-20 23:48:50 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ipmd",
-    version="0.1.0",
+    version="1.2.1",
     description="Image Pixel MetaData - Pin metadata into image pixels",
     author="777Tu",
     packages=find_packages(),
